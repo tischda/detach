8f7318e4345a0832885e2a6aa80b0bb7e0f50d0a
Daniel TISCHER <dos.7182@gmail.com>
2025-09-06 23:25:32 +0200

    PsModulePath also needs to be merged

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [v1.1.0] - ?? September 2025
+
+PsModulePath also needs to be merged
+
 ## [v1.0.0] - 31 August 2025
 
 First version
--- env.go
+++ env.go
@@ -4,17 +4,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strings"
-	"syscall"
-	"unsafe"
 
 	"golang.org/x/sys/windows/registry"
 )
-
-var modkernel32 = syscall.NewLazyDLL("kernel32.dll")
-var procExpandEnvironmentStringsW = modkernel32.NewProc("ExpandEnvironmentStringsW")
 
 // getUserAndSystemEnv retrieves the current environment and overrides possibly stale values with
 // USER and SYSTEM environment variables from the Windows registry. The "Path" variable is merged
@@ -54,7 +48,7 @@ func getUserAndSystemEnv() ([]string, error) {
 		userEnv, _ := userReg.ReadValueNames(0)
 		for _, name := range userEnv {
 			val, _, _ := userReg.GetStringValue(name)
-			if name == "Path" {
+			if name == "Path" || name == "PsModulePath" {
 				// Append USER Path to SYSTEM Path (system first, then user)
 				envMap[name] = envMap[name] + ";" + val
 			} else {
@@ -70,29 +64,4 @@ func getUserAndSystemEnv() ([]string, error) {
 		env = append(env, fmt.Sprintf("%s=%s", k, v))
 	}
 	return env, nil
-}
-
-// expandVariable returns the resolved value of environment variables containing
-// other variables such as %APPDATA% or %USERPROFILE%.
-//
-// Parameters:
-//   - v: The environment variable to expand.
-func expandVariable(v string) string {
-	src, err := syscall.UTF16PtrFromString(v)
-	if err != nil {
-		log.Fatalln("String with NULL passed to StringToUTF16Ptr")
-	}
-	buf := make([]uint16, 32767) // Maximum environment variable size on Windows
-	dst := &buf[0]
-	size := uintptr(len(buf))
-
-	n, _, _ := procExpandEnvironmentStringsW.Call(
-		uintptr(unsafe.Pointer(src)),
-		uintptr(unsafe.Pointer(dst)),
-		size,
-	)
-	if n != 0 && n <= size {
-		v = syscall.UTF16ToString(buf[:n-1])
-	}
-	return v
 }
